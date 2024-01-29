a5c8c90c18c5225998916c7feba65a6b18e206aa
IKRAMboukri <156421583+IKRAMboukri@users.noreply.github.com>
2024-01-29 15:08:23 +0100

    test git

---
+++ session-1/index01.js
@@ -0,0 +1,35 @@
+//console.log("hello world");
+let nombreDeux;
+let nombreUn;
+let somme;
+const PI = 3.14;
+nombreUn=16;
+nombreDeux=14;
+//somme=nombreUn+nombreDeux;
+//console.log(somme)
+
+//les fonctions
+function greet(nom){
+    console.log("helloo!"+ nom)
+}
+greet("Boukri")
+greet("Bakarouch")
+greet("aitsaid")
+
+function sum(a,b){
+    return a+b
+}
+console.log(sum(12,30))
+
+function inscrire(){
+   let nom="boukri";
+   let prenom="ikram";
+   let age=19;
+
+   if (age== 19){
+    let statut ="etudiant"
+    console.log(statut)
+   }
+   
+}
+inscrire()
---
+++ session-2/index02.js
@@ -0,0 +1,44 @@
+let texte1="ofppt";
+texte1+=" ";
+let texte2="en force";
+let texte3= texte1 + texte2;
+console.log(texte3)
+
+let x=1+1;
+let y="5" +1;
+console.log(y)
+
+let a=10;
+let b="10";
+if (a===b)
+{
+    console.log("true")
+}else{
+    console.log("false")
+};
+
+let statut="etudiant";
+let age= "20";
+if (statut=="etudiant"|| age===20){
+    console.log("ok")
+}else if(statut == "employé"){
+    console.log("error")
+}
+
+//let statut = false;
+//let age = 20;
+if(!statut){
+    console.log("okk")
+}
+
+
+let valide= false;
+if (!valide){//false=false
+    console.log("pas admis")
+
+}
+
+
+
+
+
